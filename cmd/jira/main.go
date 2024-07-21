0b19a88354e04693bfaa0ba7862387d7418ac899
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-07-21 13:27:13 +0100

    feat(db): add required framework for using immudb

--- cmd/jira/main.go
+++ cmd/jira/main.go
@@ -2,9 +2,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	immudb "github.com/codenotary/immudb/pkg/client"
 )
 
+type Example struct {
+	ID   int `gorm:"primarykey"`
+	Name string
+}
+
 func main() {
-	fmt.Println("test")
+	opts := immudb.DefaultOptions().WithAddress("127.0.0.1").WithPort(3322)
+
+	client := immudb.NewClient().WithOptions(opts)
+
+	// connect with immudb server (user, password, database)
+	err := client.OpenSession(context.Background(), []byte("immudb"), []byte("immudb"), "defaultdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ensure connection is closed
+	defer client.CloseSession(context.Background())
+
+	fmt.Printf("connected")
 }
--- flake.nix
+++ flake.nix
@@ -3,65 +3,82 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, pre-commit-hooks }:
-    let
-      version = builtins.substring 0 1 self.lastModifiedDate;
-
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-    in {
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go-tools
-              go_1_22
-              gopls
-              gotools
-              nixpkgs-fmt
-              revive
-            ];
-          };
-        });
-
-      checks = forAllSystems (system: {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            gofmt.enable = true;
-            gotest.enable = true;
-            govet.enable = true;
-            revive.enable = true;
-          };
-        };
-      });
-
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in rec {
-          bin = pkgs.buildGoModule {
-            pname = "jira-clone";
-            inherit version;
-            src = ./.;
-            vendorHash = null;
-          };
-
-          docker = pkgs.dockerTools.buildLayeredImage {
-            name = "jira-clone";
-            tag = "latest";
-            created = "now";
-            config.Cmd = [ "${bin}/bin/jira" ];
-          };
-        });
-
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, gomod2nix }:
+  (flake-utils.lib.eachDefaultSystem
+  (system:
+  let
+    pkgs = import nixpkgs {
+    inherit system;
+    overlays = [
+    gomod2nix.overlays.default
+    ];
     };
-}
+
+    version = builtins.substring 0 1 self.lastModifiedDate;
+
+    bin = pkgs.buildGoApplication {
+      pname = "jira-clone";
+      inherit version;
+      src = ./.;
+      vendorHash = null;
+      modules = ./gomod2nix.toml;
+    };
+
+    docker = pkgs.dockerTools.buildImage {
+      name = "jira-clone";
+      tag = "latest";
+      created = "now";
+      config.Cmd = [ "${bin}/bin/jira" ];
+    };
+
+  in
+  {
+      packages = {
+        bin = bin;
+        docker = docker;
+      };
+
+
+  checks = {
+    pre-commit-check = pre-commit-hooks.lib.${system}.run {
+      src = ./.;
+      hooks = {
+        gofmt.enable = true;
+        gotest.enable = true;
+        govet.enable = true;
+        revive.enable = true;
+      };
+    };
+  };
+
+      devShell = pkgs.mkShell {
+      #For some insane reason this doesn't work using the `with pkgs;` syntax
+        packages = [
+          pkgs.gomod2nix
+        ];
+
+        buildInputs = [
+          pkgs.go-tools
+          pkgs.go_1_22
+          pkgs.gopls
+          pkgs.gotools
+          pkgs.nixpkgs-fmt
+          pkgs.revive
+          pkgs.immudb
+        ];
+      };
+
+  })
+  );
+
+  }
